056431b7a01ec98a8cad9a337a800c252567522a
Mica CF <mcohenfuentes@gmail.com>
2025-01-23 15:48:09 -0500

    exercise 3 and 4 complete

--- app.js
+++ app.js
@@ -40,3 +40,47 @@ function isAdult(number) {
 
 
 console.log('Exercise 2 Result:', isAdult(21));
+
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+function isCharAVowel(char) {
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+       return true;
+    } else {
+        return false;
+    }
+}
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+function generateEmail(name, domain) {
+    return `${name}@${domain}`
+}
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
