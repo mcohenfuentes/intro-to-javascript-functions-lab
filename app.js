288b541deb62fd1b84fe551d9525a9064e4b553b
Mica CF <mcohenfuentes@gmail.com>
2025-01-23 19:41:54 -0500

    need to complete exercise 8 and 9

--- app.js
+++ app.js
@@ -84,3 +84,114 @@ function generateEmail(name, domain) {
 }
 
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+ function greetUser(name, timeOfDay) {
+    return `Good ${timeOfDay}, ${name}!`
+ }
+
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+function maxOfThree(num1, num2, num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1 
+    }
+    else if (num2 >= num1 && num2 >= num3) {
+        return num2
+    }
+    else {
+        return num3
+    }
+}
+
+console.log(maxOfThree(5, 10, 8));
+
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+function calculateTip(billAmount, tipPercentage) {
+    console.log(billAmount * tipPercentage / 100)
+}
+
+
+console.log(calculateTip(50, 20));
+
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+function convertTemperature(temperature, scale) {
+    console.log(temperature, scale)
+    // change temperature number based of scale given
+    let convertedTemperature = 0
+    convertedTemperature = (temperature * 9/5) + 32
+    console.log(convertedTemperature)
+}
+convertTemperature(32, 'F')
+
+//console.log('Exercise 8 Result:', convertTemperature(34, "F"));
+
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+
+
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
